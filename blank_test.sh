66a6340d4d3972c758a7c4e02ecb71fed1863b80
Sraleik <nicolas.rotier@gmail.com>
2017-03-02 13:39:03 +0100

    test if sudo > 1.8.0

--- blank_test.sh
+++ blank_test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function version { echo "$@" | gawk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
+
+if [ "$(version "$(sudo --version | head -n1 | cut -d" " -f3)")" -gt "$(version "1.8.0")" ]; then
+  
+  echo "$(sudo --version | head -n1 | cut -d" " -f3) is greater than 1.8.0 !"
+       
+fi
+
 sudo --version
 git --version
 curl --version
